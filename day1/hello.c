73c27e4976020c159a8cf9901128bfde74023635
Bocioanca Alexandru-Teodor <alebocioanca123@gmail.com>
2023-08-10 14:46:54 +0300

    bash: Add 2 bash script files
    - Added one bash script file for each task

--- day1/hello.c
+++ day1/hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-
-	printf("Hello Analog Devices students\n");
+	int n;
+	n = 20;
+	for(int i=1; i<=n; i++)
+	{
+		printf("%d\n", i);
+		sleep(1);
+	}
 	return 0;
 }
